3ec8891883e3e4bae0177e23121b631fb3920fdf
andrejeronimo <andre.jeronimo.93@gmail.com>
2014-04-29 12:23:58 +0100

    Páginas Home e CriarPerfil
    Poderão precisar de uns ajustes no tamanho da janela depois.

---
+++ dr_pocket_application/dr_pocket_application/Home1.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApplication1
+{
+    /// <summary>
+    /// Interaction logic for Home1.xaml
+    /// </summary>
+    public partial class Home1 : Page
+    {
+        public Home1()
+        {
+            InitializeComponent();
+        }
+
+        private void button_create_profile_Click(object sender, RoutedEventArgs e)
+        {
+            // View Create Profile Page
+            CriarPerfil createProfilePage = new CriarPerfil();
+            this.NavigationService.Navigate(createProfilePage);
+        }
+    }
+}
